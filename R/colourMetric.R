af96bb24c1c8f6efeac3eb13c7849c579f1e2099
Paul Murrell <paul@stat.auckland.ac.nz>
2018-01-12 13:29:19 +1300

    changed ISCCNBSblock() to return colours from same block when colour list is not ISCCNBScolours

--- R/colourMetric.R
+++ R/colourMetric.R
@@ -5,14 +5,6 @@
 ISCCNBSnames <- readRDS(system.file("extdata", "block.rds",
                                     package="rolocISCCNBS"))
 
-LUVcoords <- function(sRGB) {
-    luv <- as(sRGB, "LUV")
-    coords <- coords(luv)
-    ## Special case "black" to avoid NAs (set U and V to 0)
-    coords[coords[,"L"] == 0, ] <- 0
-    coords
-}
-
 ISCCNBSblock <- function(colour, colourList) {
     ## Convert colours to block name
     ## This gives us which block the colour is within
@@ -31,13 +23,9 @@ ISCCNBSblock <- function(colour, colourList) {
         listBlockNames <- ISCCNBSnames[round(listRGB[,1]*255) +
                                        round(listRGB[,2]*255)*256 +
                                        round(listRGB[,3]*255)*256*256 + 1]
-        ## For each colour, if in same block as list colour,
-        ## compute distance between colour and list colour
+        ## 0 if colour is in same block as colour list,
         ## Inf for everything else
-        specCoords <- LUVcoords(colour)
-        listCoords <- LUVcoords(colourList)
-        distances <- t(apply(specCoords, 1, euclideanDistance, listCoords))
         t(outer(listBlockNames, blockNames,
-                function(x, y) ifelse(x == y, distances, Inf)))
+                function(x, y) ifelse(x == y, 0, Inf)))
     }
 }
